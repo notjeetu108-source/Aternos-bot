1d7ebf0183752fd705143c8b6a693822a523e8b7
notjeetu108-source <notjeetu108@gmail.com>
2026-08-21 07:58:05 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+aternose bot
+const http = require('http');
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Aternos Bot is running!');
+});
+server.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
